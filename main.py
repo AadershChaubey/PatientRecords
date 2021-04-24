824a5718eecc094cd231be7556de1c16be092b71
jackchenry <jackchenry97@gmail.com>
2021-04-24 14:27:38 +0100

    Running page

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from flask import Flask
+from flask import request, escape, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
-def home():
-    return "Hello, World!"
-    
+def index():
+    return render_template("home.html")
+
+def findPatient():
+    name = str(escape(request.args.get("patientName", "")))
+    print(name)
+
+@app.route("/<int:findpatient>")
+def patientDataFrom(name):
+    """Gets patient data."""
+    data = 1
+    return str(data)
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
